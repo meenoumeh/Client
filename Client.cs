f0a97f2de699965d0a2c19a48a7d8d8f688d1fb6
meenoumeh <67861041+meenoumeh@users.noreply.github.com>
2022-01-16 20:04:41 +0330

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    public class Client
+    {
+        public IPAddress MyIP { get; private set; }
+        public int Port { get; private set; }
+        public bool Status = true;
+        public TcpClient TcpClient { get; set; }
+        public NetworkStream NetworkStream { get; set; }
+        public StreamReader StreamReader { get; set; }
+        public StreamWriter StreamWriter { get; set; }
+        public Socket ServerSocket { get; set; }
+
+        public Client(IPAddress iPAddress, int port)
+        {
+            MyIP = iPAddress;
+            Port = port;
+        }
+
+        public void ConnectToServer()
+        {
+            try
+            {
+                TcpClient = new TcpClient(MyIP.ToString(), Port);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ServerData()
+        {
+            NetworkStream = TcpClient.GetStream();
+            StreamReader reader = new StreamReader(NetworkStream);
+            StreamWriter writer = new StreamWriter(NetworkStream);
+        }
+
+        public void Disconnect()
+        {
+            StreamWriter.Close();
+            StreamReader.Close();
+            NetworkStream.Close();
+            TcpClient.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.1.70");
+            int port = 3000;
+            Client client = new Client(ip, port);
+            Console.WriteLine("connected successfully ....");
+            Thread.Sleep(500);
+            Console.Clear();
+            client.ServerData();
+
+            try
+            {
+                string messageToServer = "";
+                string messageFromServer = "";
+
+                while (client.Status)
+                {
+                    messageToServer = Console.ReadLine();
+                    if (messageToServer == "exit")
+                    {
+                        client.Status = false;
+                        client.StreamWriter.WriteLine("bye");
+                        client.StreamWriter.Flush();
+
+                    }
+                    else
+                    {
+                        client.StreamWriter.WriteLine(messageToServer);
+                        client.StreamWriter.Flush();
+
+                        messageFromServer = client.StreamReader.ReadLine();
+                        Console.WriteLine("server : " + messageFromServer);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
